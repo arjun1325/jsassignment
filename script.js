f444bc0b29ea14bc7d584fc4ec20a72569703822
Arjun M <arjunajuoff@gmail.com>
2024-06-09 15:16:25 +0530

    created a branch and calculated area of a rectangle

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-let a = prompt("enter a digit to add")
-let b = prompt("enter second digit")
-let sum= parseInt(a)+ parseInt(b);
-alert("the sum of two numbers is" +sum)
+// let a = prompt("enter a digit to add")
+// let b = prompt("enter second digit")
+// let sum= parseInt(a)+ parseInt(b);
+// alert("the sum of two numbers is" +sum)
+
+// Area of a rectangle 
+
+let l= 5
+console.log("length =", l);
+let w=10
+console.log("height =" , w);
+let area = l*w
+console.log("area of the rectangle is length x width");
+console.log("i.e area of the rectangle " , area)
